fd992722e7d9b2301d7d429bbbb78dfc1e721715
Patrick Sacchet <psacchet@proton.me>
2026-06-12 19:23:58 -0400

    Add baseline insert to native detours dll

---
+++ Win-Detours/Detours-Native/Detours-Native.cpp
@@ -0,0 +1,11 @@
+// Main functionality we'll export for our main exe to utilize 
+	// Allows users to play around in their own enviornment/executable 
+
+#include "Detours-Native.h"
+
+BOOL DoNativeDetours()
+{
+	printf("In detours native\n");
+
+	return TRUE;
+}
--- Win-Detours/Win-Detours.cpp
+++ Win-Detours/Win-Detours.cpp
@@ -1,31 +1,71 @@
+// Main 'binary' for detours functionality
+    // Will load supplemental DLLs (users can modify/play around with if desired) for each different scenario one would use detours to add custom callbacks
+    // Scenarios:
+        // Detour own executable with defined callbacks to certain APIs
+        // Modify the export table of a existing binary to use custom callbacks 
+        // Launch a new instance of a binary with custom callbacks
+        // Inject our detour'd functionality into an already running executable 
+
 
 #include "Win-Detours.h"
 
+typedef BOOL(__stdcall* DoNativeDetours)(); // exported from Detours-Native.dll
+
 
 // Call into our pre-built DLL that will display the baseline for using these custom callbacks 
-BOOL handleNativeDetours()
+BOOL HandleNativeDetours()
 {
+    HMODULE detoursNativeLib;
+    DoNativeDetours doNativeDetours;
+
     // This functionality is held in our Detours-Native DLL so load that 
+        // This DLL is a dependency so the path should be fine.... its also relative
+        // TODO: detours really only does x86 so assume user will provide x86 debug or handle all cases
+    detoursNativeLib = LoadLibrary(L"Detours-Native.dll");
+    if (detoursNativeLib == NULL)
+    {
+        printf("ERROR; Failed LoadLibrary; error 0x%X\n", GetLastError());
+        return FALSE;
+    }
+
+    doNativeDetours = GetProcAddress(detoursNativeLib, "DoNativeDetours");
+    if (doNativeDetours == NULL)
+    {
+        printf("ERROR; Failed GetProcAddressl error 0x%X\n", GetLastError());
+        return FALSE;
+    }
+
+    if (!doNativeDetours())
+    {
+        printf("ERROR Failed DoNativeDetours\n");
+        return FALSE;
+    }
+
+    if (!FreeLibrary(detoursNativeLib))
+    {
+        printf("ERROR; Failed FreeLibrary; error 0x%X\n", GetLastError());
+        return FALSE;
+    }
 
     return TRUE;
 }
 
 // Modify an existing binary via DetourBinaryEditImports()
-BOOL handleModifyBinaryDetours()
+BOOL HandleModifyBinaryDetours()
 {
 
     return TRUE;
 }
 
 // Create a new instance of a binary via DetourCreateProcessWithDllEx()
-BOOL handleNewProcessDetours()
+BOOL HandleNewProcessDetours()
 {
 
     return TRUE;
 }
 
 // Inject into an already running process and attempt to use our callbacks that way
-BOOL handleInjectProcessDetours()
+BOOL HandleInjectProcessDetours()
 {
 
 
@@ -33,7 +73,7 @@ BOOL handleInjectProcessDetours()
 }
 
 // Print some help for the user 
-void printHelp()
+void PrintHelp()
 {
     printf("Please select one of the following to explore Detours capabilitites:\n");
     printf("\t0) Exit\n");
@@ -52,7 +92,7 @@ int main()
 
     while (!exit)
     {
-        printHelp();
+        PrintHelp();
 
         std::getline(std::cin, choice);
 
@@ -66,7 +106,7 @@ int main()
         // User wants to just use Detours contained within its own program
         case NATIVE:
             printf("Executing native Detours functionality...\n");
-            if (!handleNativeDetours())
+            if (!HandleNativeDetours())
             {
                 printf("ERROR; Failed handleNativeDetours; exiting...\n");
                 exit = TRUE;
@@ -79,7 +119,7 @@ int main()
             }
         case MODIFY:
             printf("Executing modification of external binary...\n");
-            if (!handleModifyBinaryDetours())
+            if (!HandleModifyBinaryDetours())
             {
                 printf("ERROR; Failed handleModifyBinaryDetours; exiting...\n");
                 exit = TRUE;
@@ -92,7 +132,7 @@ int main()
             }
         case INSTANCE:
             printf("Executing a new instance of a executable with detour'd functionality...\n");
-            if (!handleNewProcessDetours())
+            if (!HandleNewProcessDetours())
             {
                 printf("ERROR; Failed handleNewProcessDetours; exiting...\n");
                 exit = TRUE;
@@ -105,7 +145,7 @@ int main()
             }
         case INJECT:
             printf("Attemtping to inject detuor'd functionality into running process...\n");
-            if (!handleInjectProcessDetours())
+            if (!HandleInjectProcessDetours())
             {
                 printf("ERROR; Failed handleInjectProcessDetours; exiting...\n");
                 exit = TRUE;
